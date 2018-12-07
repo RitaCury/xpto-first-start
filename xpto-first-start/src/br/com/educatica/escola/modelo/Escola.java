dda5652109f5a57930525a08720d7d2cd13c786f
ritac <ritac@LAPTOP-7KEAUH47>
2018-12-07 15:33:02 -0200

    Primeiro commit

--- xpto-first-start/src/br/com/educatica/escola/modelo/Escola.java
+++ xpto-first-start/src/br/com/educatica/escola/modelo/Escola.java
@@ -2,6 +2,17 @@ package br.com.educatica.escola.modelo;
 
 public class Escola {
 	private String nome;
+	private String telefone;
+	
+	
+	
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
 	public String getNome() {
 		return nome;
