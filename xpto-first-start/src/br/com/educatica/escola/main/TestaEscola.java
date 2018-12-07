0b788dcb603d518666e1884b7184bf753857198c
ritac <ritac@LAPTOP-7KEAUH47>
2018-12-07 15:17:04 -0200

    Primeiro commit

---
+++ xpto-first-start/src/br/com/educatica/escola/main/TestaEscola.java
@@ -0,0 +1,12 @@
+package br.com.educatica.escola.main;
+
+import br.com.educatica.escola.modelo.Escola;
+
+public class TestaEscola {
+
+	public static void main(String[] args) {
+		Escola escola = new Escola();
+		escola.setNome("XPTO");
+	}
+
+}
